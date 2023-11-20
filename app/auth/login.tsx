9906c3773e5bfeb16c4d12161db4a7e6e52494b4
Charaf Alade YESSOUFOU <aladecharaf23@gmail.com>
2023-11-20 20:18:16 +0100

    feat add login page

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,13 +1,38 @@
-import { View , TextInput} from "react-native";
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function LoginScreen() {
-    return (
-        <View className="flex-1 justify-center items-center px-3 ">
-           <View> 
-                <View className="w-full rounded-sm border border-purple-800 bg-purple-50">
-                </View>
-                <TextInput className="" /> 
-            </View>
-        </View>
-    );
-}
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 bg-white p-4">
+      <View className="flex-1 items-center justify-center">
+        <Text className="font-bold text-3xl text-gray-800">Login</Text>
+      </View>
+
+      <View className="mt-8">
+        <TextInput
+          className="bg-gray-200 p-2 rounded-md w-full"
+          placeholder="Email"
+        />
+      </View>
+
+      <View className="mt-4">
+        <TextInput
+          className="bg-gray-200 p-2 rounded-md w-full"
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+      </View>
+
+      <View className="mt-8">
+        <Button title="Login" className="bg-blue-500 text-white p-2 rounded-md w-full" />
+      </View>
+
+      <View className="mt-4 flex-row justify-between">
+        <Text className="text-gray-500">Forgot Password?</Text>
+        <Text className="text-gray-500">Sign Up</Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
