325b295057f47c1b63a0d66aa88771610aaded38
Charaf Alade YESSOUFOU <aladecharaf23@gmail.com>
2023-11-20 17:41:28 +0100

    first commit

---
+++ app/auth/login.tsx
@@ -0,0 +1,13 @@
+import { View , TextInput} from "react-native";
+
+export default function LoginScreen() {
+    return (
+        <View className="flex-1 justify-center items-center px-3 ">
+           <View> 
+                <View className="w-full rounded-sm border border-purple-800 bg-purple-50">
+                </View>
+                <TextInput className="" /> 
+            </View>
+        </View>
+    );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,28 @@
 import { Link } from 'expo-router';
+import { Image } from 'expo-image';
 import { Text, TouchableOpacity, View } from 'react-native';
+
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function Page() {
   return (
     <View className={styles.container}>
+      <Image
+        className='h-full w-full absolute inset-0'
+        source={require('../assets/onboarding.png')}
+        contentFit="cover"
+        transition={1000}
+        placeholder={blurhash}
+      />
       <View className={styles.main}>
         <View>
           <Text className={styles.title}>Hello World</Text>
-          <Text className={styles.subtitle}>This is the first page of your app.</Text>
+          <Text className={styles.subtitle}>Onboarding</Text>
         </View>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+        <Link href="auth/login" asChild>
           <TouchableOpacity className={styles.button}>
-            <Text className={styles.buttonText}>Show Details</Text>
+            <Text className={styles.buttonText}>Login</Text>
           </TouchableOpacity>
         </Link>
       </View>
@@ -20,9 +31,9 @@ export default function Page() {
 }
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  button: 'items-center bg-indigo-600 rounded-[28px] shadow-md p-4',
   buttonText: 'text-white text-lg font-semibold text-center',
-  container: 'flex-1 p-6',
+  container: 'flex-1 p-6 relative',
   main: 'flex-1 max-w-[960] justify-between',
   title: 'text-[64px] font-bold',
   subtitle: 'text-4xl text-gray-700',
